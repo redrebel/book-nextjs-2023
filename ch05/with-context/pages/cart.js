0d4eaee22b4479665c7a2f63fa4b7ae438626e7f
이정근 <junggelee@gmarket.com>
2023-10-16 19:52:23 +0900

    20231016 with context

--- ch05/with-context/pages/cart.js
+++ ch05/with-context/pages/cart.js
@@ -2,17 +2,28 @@
 import { useContext } from 'react';
 import cartContext from "../components/context/cartContext";
 import data from '../data/items';
+import items from "../data/items";
+
+function getFullItem(id) {
+    const idx = data.findIndex((item) => item.id === id);
+    return data[idx];
+}
 
 function Cart() {
-    const { items } = useContext(cartContext);
+  const { items } = useContext(cartContext);
+  const total = Object.keys(items)
+      .map((id) => getFullItem(id).price * items[id])
+      .reduce((x, y) => x + y, 0);
 
+  const amounts = Object.keys(items).map((id) => {
+    const item = getFullItem(id);
+    return { item, amount: items[id]};
+  })
   return (
     <div>
-      <h1 className="text-xl font-bold"> Total: ${/* To be implemented */} </h1>
+      <h1 className="text-xl font-bold"> Total: ${total} </h1>
       <div>
-        {[
-          /* To be implemented */
-        ].map(({ item, amount }) => (
+        {amounts.map(({ item, amount }) => (
           <div key={item.id}>
             x{amount} {item.name} (${amount * item.price})
           </div>
